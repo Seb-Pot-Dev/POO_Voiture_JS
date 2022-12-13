db0daaea67938573bc7e76d4c15a15c9de2f91cc
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2022-12-13 16:52:17 +0100

    Add comment to script and Alert when wrong button

--- Car.js
+++ Car.js
@@ -6,19 +6,23 @@ class Car {
     this._statut = false;
     this._actualspeed = 0;
   }
+  // START
   start() {
     if (this._statut == false) {
       this._statut = true;
       document.getElementById("start").innerHTML = this + " is starting...";
       console.log(this + " is starting...");
     } else {
+      alert(this + " is already started !");
       console.log(this + " is already started !");
     }
   }
+  // ACCELERATE
   accelerate(speed) {
     if (this._statut == true) {
-        this._actualspeed += speed;
-      document.getElementById("speed").innerHTML= this+" is accelerating to "+this._actualspeed;
+      this._actualspeed += speed;
+      document.getElementById("speed").innerHTML =
+        this + " is accelerating to " + this._actualspeed;
       console.log(
         this +
           " accelerate of " +
@@ -28,13 +32,16 @@ class Car {
           " km/h"
       );
     } else {
+      alert(this + " is off. Please start before accelerating.");
       console.log(this + " is off. Please start before accelerating.");
     }
   }
+  // SLOW
   slow(speed) {
     if (this._actualspeed > 0) {
       this._actualspeed -= speed;
-      document.getElementById("slow").innerHTML= this+" is slowing down to "+this._actualspeed;
+      document.getElementById("slow").innerHTML =
+        this + " is slowing down to " + this._actualspeed;
       console.log(
         this +
           " is slowing down of " +
@@ -44,10 +51,26 @@ class Car {
           " km/h"
       );
     } else {
+      alert(
+        this +
+          " doesn't have a speed at the moment. Please accelerate before slowing down."
+      );
       console.log(
         this +
           " doesn't have a speed at the moment. Please accelerate before slowing down."
       );
+    }
+  }
+  // STOP
+  stop() {
+    if (this._statut == true) {
+      this._statut = false;
+      this._actualspeed = 0;
+      document.getElementById("stop").innerHTML = this + " does stop !";
+      console.log(this + " is now turned off.");
+    } else {
+      alert(this + " is already stopped.");
+      console.log(this + " is already stopped.");
     }
   }
   get_statut() {
@@ -57,26 +80,7 @@ class Car {
       console.log(this + " is off.");
     }
   }
-  stop() {
-    if (this._statut == true) {
-      this._statut = false;
-      this._actualspeed = 0;
-      document.getElementById("stop").innerHTML = this + " does stop !";
-      console.log(this + " is now turned off.");
-    } else {
-      console.log(this + " is already stopped.");
-    }
-  }
-
-  toString() {
-    return this._brand + " " + this._model;
-  }
-  showModel() {
-    document.getElementById("showmodel").innerHTML = //Avec getElementById on cherche un élément dans l'html qui possède un id précis, puis on place dans son code html (innerHTML) les éléments suivants le =
-      "Model is : " + this._model + ".";
-
-    console.log("Model is : " + this._model + ".");
-  }
+  // GETTERS
   get_all_infos() {
     return this + " got " + this._nbdoors + " doors";
   }
@@ -91,5 +95,18 @@ class Car {
   }
   get_actual_speed() {
     return this._actualspeed;
+  }
+
+  // SHOW
+  showModel() {
+    document.getElementById("showmodel").innerHTML = //Avec getElementById on cherche un élément dans l'html qui possède un id précis, puis on place dans son code html (innerHTML) les éléments suivants le =
+      "Model is : " + this._model + ".";
+
+    console.log("Model is : " + this._model + ".");
+  }
+
+  //TO STRING
+  toString() {
+    return this._brand + " " + this._model;
   }
 }
